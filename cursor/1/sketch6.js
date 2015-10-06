b8819ea13c78b4b7610bc3d13f457f5016f810f0
valerienel123@yahoo.com <valerienel123@yahoo.com>
2015-10-06 13:20:02 -0400

    a

---
+++ cursor/1/sketch6.js
@@ -0,0 +1,83 @@
+
+var brush1
+
+
+
+
+
+
+
+function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+    noCursor();
+    brush1 = loadImage("cur330.png");
+   
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function draw() {
+    background (255,255,255,100)
+    noStroke();
+    fill(10,70,255)
+    textSize(18);
+    text("enter", 300, 530);
+    //rect(300,500,40,40);
+    
+    
+  
+    /*  
+        <, <=, >, >=, ==, &&, ||
+    */
+
+    //imageMode(CENTER)
+    image(brush1, mouseY, mouseX, 40, 40);
+   //image(brush1, mouseY-100, mouseX+20, 1, 1)
+    
+
+    
+    
+}
+
+
+
+function mousePressed() {
+    
+//image1
+    if (mouseX > 485 && mouseX < 527 && mouseY > 285 && mouseY < 327) {
+       background (255,100,255,100)
+       window.location.href = "http://www.valerie-nelson.com/cursor/2";
+    }
+}
+    
+
+    
+    
+    
+ //image2   
+ //if (mouseX+20 > 500 && mouseX+20 < 530 && mouseY-100 > 300 && mouseY-100 < 330) {
+       
+  //     alert("ay!")
+       //show "1" point, move the box, and switch cursor
+ //  }
+
+
+
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
--- cursor/sketch7.js
+++ cursor/sketch7.js
@@ -2,6 +2,11 @@
 var brush1
 
 
+
+
+
+
+
 function setup() {
 
     createCanvas(windowWidth, windowHeight);
@@ -12,11 +17,23 @@ function setup() {
 }
 
 
+
+
+
+
+
+
+
+
+
+
 function draw() {
     background (255,255,255,100)
     noStroke();
     fill(10,70,255)
-    rect(300,500,40,40);
+    textSize(18);
+    text("enter", 300, 530);
+    //rect(300,500,40,40);
     
     
   
@@ -25,7 +42,7 @@ function draw() {
     */
 
     //imageMode(CENTER)
-    image(brush1, mouseY, mouseX, 40, 40);
+    image(brush1, mouseX, mouseY, 40, 40);
    //image(brush1, mouseY-100, mouseX+20, 1, 1)
     
 
@@ -38,9 +55,9 @@ function draw() {
 function mousePressed() {
     
 //image1
-    if (mouseX > 485 && mouseX < 527 && mouseY > 285 && mouseY < 327) {
+    if (mouseX > 285 && mouseX < 327 && mouseY > 485 && mouseY < 527) {
        background (255,100,255,100)
-       window.location.href = "http://www.valerie-nelson.com/cursor/2";
+       window.location.href = "http://www.valerie-nelson.com/cursor/1";
     }
 }
     

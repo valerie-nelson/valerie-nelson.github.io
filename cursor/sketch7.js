e18cd36d1f4fb702f4950e54124dd886463e44a6
valerienel123@yahoo.com <valerienel123@yahoo.com>
2015-10-15 19:48:05 -0400

    r

--- cursor/sketch7.js
+++ cursor/sketch7.js
@@ -11,7 +11,7 @@ function setup() {
 
     createCanvas(windowWidth, windowHeight);
     noCursor();
-    brush1 = loadImage("/cur330.png");
+    brush1 = loadImage("cur330.png");
    
  
 }
@@ -57,7 +57,7 @@ function mousePressed() {
 //image1
     if (mouseX > 185 && mouseX < 227 && mouseY > 285 && mouseY < 327) {
        background (255,100,255,100)
-       window.location.href = "http://www.valerie-nelson.com/cursor/1";
+       window.location.href = "http://valerie-nelson.github.io/cursor/1";
     }
 }
     
